071f6ff78c846e124c513170356bb39d729df72f
Tomás Sandoval <71939152+TomySand@users.noreply.github.com>
2023-12-14 22:21:05 -0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -22,27 +22,4 @@ class Camera():
         if self.__player.get_rect.bottom > self.__limity[1]:
             self.__movement[1] = self.__limity[1] - self.__player.get_rect.bottom
             self.__player.get_rect.bottom = self.__limity[1]
-        
-    #     if self.rect.left < constants.SCROLL_THRESH:
-    #         screen_scroll[0] = constants.SCROLL_THRESH - self.rect.left
-    #         self.rect.left = constants.SCROLL_THRESH
-    #     if self.rect.right > (constants.SCREEN_WIDTH - constants.SCROLL_THRESH):
-    #         screen_scroll[0] = (constants.SCREEN_WIDTH - constants.SCROLL_THRESH) - self.rect.right
-    #         self.rect.right = constants.SCREEN_WIDTH - constants.SCROLL_THRESH
-        
-
-    #   #move camera up and down
-    #     if self.rect.top < constants.SCROLL_THRESH:
-    #         screen_scroll[1] = constants.SCROLL_THRESH - self.rect.top
-    #         self.rect.top = constants.SCROLL_THRESH
-    #     if self.rect.bottom > (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH):
-    #         screen_scroll[1] = (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH) - self.rect.bottom
-    #         self.rect.bottom = constants.SCREEN_HEIGHT - constants.SCROLL_THRESH
-        
-        
         return self.__movement
-    
-set_score  = 1000
-score = set_score 
-set_score = 0
-print(score)
